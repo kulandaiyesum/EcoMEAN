0f72f4b468dc70aa933adcee4717ac355e359483
kulandaiyesu <kulandaiyesu@gmail.com>
2024-06-03 17:59:30 +0530

    fix: Register validation password

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -39,9 +39,9 @@ export class RegisterComponent implements OnInit {
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
-        Validators.pattern(
-          '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
-        ),
+        // Validators.pattern(
+        //   '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
+        // ),
       ]),
       confirmPassword: ['', Validators.required, confirmPasswordValidator],
     });
