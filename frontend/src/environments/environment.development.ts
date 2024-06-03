a58b5f33fa7c0563604c2c860cc871a87e0f2be7
kulandaiyesu <kulandaiyesu@gmail.com>
2024-06-03 17:52:51 +0530

    fix:API endpoint update

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://ecomean.onrender.com/api',
   razorpayKeyId: 'rzp_test_4dB3cTILRxmn96',
 };
